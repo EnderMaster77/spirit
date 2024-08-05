097a65d14f78a75e45d4be6ecbf543d9ce1534df
EnderMaster77 <javier.jgonzalez@protonmail.com>
2024-08-04 23:49:58 -0400

    Re-arranged files for better organization.

--- Scripts/camera_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Camera2D
-
-@onready var player: CharacterBody2D = $".."
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print(player.velocity)
-	offset = lerp(offset, player.velocity, delta * 4)
--- Scripts/switch_gui_element.gd
+++
@@ -1,3 +0,0 @@
-extends Sprite2D
-class_name ElementSwitchGuiItem
-@export var element_to_switch_to: String = "neutral"
