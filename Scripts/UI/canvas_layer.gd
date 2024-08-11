28c80c970041c2a939a51cc2437fb36e7e22fa9e
EnderMaster77 <javier.jgonzalez@protonmail.com>
2024-08-10 22:45:16 -0400

    Revamped debug menu.

--- Scripts/UI/canvas_layer.gd
+++ Scripts/UI/canvas_layer.gd
@@ -1,18 +1,18 @@
 extends CanvasLayer
 
+@export var player: CharacterBody2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	show()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Panel/Velocity.text = "Velocity: " + str($"..".velocity)
-	$Panel/FPS.text = "FPS: " + str(Engine.get_frames_per_second())
-	#$Panel/mainMod.text = "Main Mod: " + str($"../SpriteManager/Main".get_modulate())
-	#$Panel/transMod.text = "Trans Mod: " + str($"../SpriteManager/Transition_To".get_modulate())
-
+	$PanelContainer/ItemList/pos.text = "Position: " + str(player.global_position)
+	$PanelContainer/ItemList/vel.text = "Velocity: " + str(player.velocity)
+	$PanelContainer/ItemList/fps.text = "FPS: " + str(Engine.get_frames_per_second())
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("showDebug"):
